b77055458747e6b20845bfa6e6dddc0dddff1ea4
spynos <youngsoo.brain@gmail.com>
2019-04-14 11:42:55 +0900

    detection face

--- pose_app/urls.py
+++ pose_app/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
-from . import views  # add
+from . import views
+
 from django.conf import settings  # add
 from django.conf.urls.static import static  # add
 
 urlpatterns = [
     url(r'^$', views.first_view, name='first_view'),
     url(r'^uimage/$', views.uimage, name='uimage'),  # add
+    url(r'^dface/$', views.dface, name='dface'),  # add
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # add
--- pose_app/views.py
+++ pose_app/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from .forms import UploadImageForm
 from django.core.files.storage import FileSystemStorage
-
+from .forms import ImageUploadForm
+from django.conf import settings
+from .opencv_dface import opencv_dface
+from .models import ImageUploadModel
 
 def first_view(request):
     return render(request, 'pose_app/first_view.html', {})
@@ -22,3 +25,19 @@ def uimage(request):
     else:
         form = UploadImageForm()
         return render(request, 'pose_app/uimage.html', {'form': form})
+
+
+def dface(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+
+            imageURL = settings.MEDIA_URL + form.instance.document.name
+            opencv_dface(settings.MEDIA_ROOT_URL + imageURL)
+
+            return render(request, 'pose_app/dface.html', {'form': form, 'post': post})
+    else:
+        form = ImageUploadForm()
+    return render(request, 'pose_app/dface.html', {'form': form})
